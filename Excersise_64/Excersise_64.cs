c02b52e2448b7af005ef67bda8686d143d982fed
Ivan K <ikolodeev002@gmail.com>
2023-03-13 15:19:03 +0200

    Final work

---
+++ Excersise_64/Excersise_64.cs
@@ -0,0 +1,17 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string CountDown (int n)
+{
+    if (n == 1) 
+    {
+        return "1";
+    }
+    string s = n.ToString() + ", " + CountDown(n-1);
+    return s;
+}
+
+Console.Write("Enter value: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(CountDown(n));
---
+++ Excersise_66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumBetween (int m, int n)
+{
+    if (n==m) return n;
+    int sum = n + SumBetween(m, n-1);
+    return sum;
+}
+
+Console.Write("Enter value M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Enter value N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+if (m >= n)
+{
+    Console.WriteLine("Can not continue: number M >= number N");
+    return;
+}
+
+int sum = SumBetween(m, n);
+Console.WriteLine($"Sum of numbes between {m} and {n} is {sum}");
